0d92fc6a7d2ff751655b4e806a0ea1959061404b
andres <andrespra71@gmail.com>
2026-07-03 10:28:09 -0500

    Agrego el MVC del Fronted Publico

---
+++ PublicFrontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PublicFrontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
